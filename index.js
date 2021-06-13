6a67ec498fc9e54dd0f6888d8c120308bca0dce7
reagan2020 <reaganteflon@.com>
2021-06-13 16:50:23 +0300

    update index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+let nameOfDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+let nameOfMales  = ["Kwasi", "Kwado","Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]
+let nameOfFemales = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]
+
+function getDetails(){
+    let year = parseInt(document.getElementById("year").value);
+    let month = parseInt(document.getElementById("month").value);
+    let date = parseInt(document.getElementById("date").value);
+    let gender = document.getElementById("gender").value;
+    //console.log(year, month, date, gender)
+    if (month < 1 || month > 12) {
+        alert("invalid month");
+    }
+
+    
+}
